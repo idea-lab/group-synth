7ea69b6fe525b21787022620b2e8cbe945f643e6
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-02-24 15:07:53 -0500

    fuck it works

--- js/gibber.js
+++ js/gibber.js
@@ -6,16 +6,17 @@ firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       currentUser = user;
     }else{
-      window.open("./login.html", "_self");
+      window.location("./login.html", "_self");
     }
   });
   firebase.database().ref('session92103/status').on('value', function(snap){
-    if(snap.val()){
+    firebaseUpdate = snap.val();
+    if(firebaseUpdate){
       doStuff();
     }else{
       $("#instructions").text("Click Anywhere to Resume");
       c.kill();
-      clearInterval(firebaseUpdate);
+      firebaseUpdate = !firebaseUpdate;
     }
   });
 document.getElementById('body').addEventListener('click', function(){
@@ -28,17 +29,20 @@ function doStuff() {
   $("#instructions").text("move your mouse");
   c = Sine(Mouse.x, Mouse.y);
   mousePos = {};
-firebaseUpdate = window.setInterval(function(){
-  $(window).mousemove(function (e) {
-    mousePos.x = e.pageX;
-    mousePos.y = e.pageY;
-  });
-  firebase.database().ref('session92103/' + currentUser.displayName).push({
-     'x' : mousePos.x,
-     'y' : mousePos.y
-  })
-  // console.log(event.clientX/window.innerWidth * 1000 + ": " + event.clientY/window.innerHeight);
-}, 1000);
+  window.setInterval(function(){
+    if(Math.floor(new Date().getTime())%1000 == 0 && firebaseUpdate){
+      $(window).mousemove(function (e) {
+        mousePos.x = e.pageX;
+        mousePos.y = e.pageY;
+      });
+      firebase.database().ref('session92103/' + currentUser.displayName).push({
+         'x' : mousePos.x,
+         'y' : mousePos.y
+      })
+      // console.log(event.clientX/window.innerWidth * 1000 + ": " + event.clientY/window.innerHeight);
+    }
+}, 10);
+
 }
 
 
