1010c7c7a911e8b9fee855125439da6e99ad8b5d
Infant Derrick Gnanasusairaj <infant.derrick.gnanasusairaj@gmail.com>
2019-02-24 15:42:04 -0500

    it probably works?

--- js/gibber.js
+++ js/gibber.js
@@ -1,6 +1,7 @@
 var currentUser;
 var firebaseUpdate;
 var c;
+var time;
 Gibber.init()
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
@@ -15,11 +16,13 @@ firebase.auth().onAuthStateChanged(function(user) {
       doStuff();
     }else{
       $("#instructions").text("Click Anywhere to Resume");
-      c.kill();
-      firebaseUpdate = !firebaseUpdate;
     }
   });
+  firebase.database().ref('session92103/startTime').on('value', function(snap){
+    time = snap.val();
+  });
 document.getElementById('body').addEventListener('click', function(){
+  firebaseUpdate = !firebaseUpdate;
   firebase.database().ref('session92103/status').once('value', function(snap){
       firebase.database().ref('session92103/status').set(!snap.val());
   });
@@ -28,24 +31,22 @@ function doStuff() {
   onStart();
   $("#instructions").text("move your mouse");
   c = Sine(Mouse.x, Mouse.y);
-  mousePos = {};
+  mousePos = {x: -1, y: -1};
   // mousePos.x = -1;
   // mousePos.y = -1;
   window.setInterval(function(){
-    if(Math.floor(new Date().getTime())%1000 == 0 && firebaseUpdate){
-      $(window).mousemove(function (e) {
-        mousePos.x = e.pageX;
-        mousePos.y = e.pageY;
-      });
-
+    $(window).mousemove(function (e) {
+      mousePos.x = e.pageX;
+      mousePos.y = e.pageY;
+    });
+    if(Math.floor(new Date().getTime()) > time && firebaseUpdate){
       firebase.database().ref('session92103/' + currentUser.displayName).push({
          'x' : mousePos.x,
          'y' : mousePos.y
       })
       // console.log(event.clientX/window.innerWidth * 1000 + ": " + event.clientY/window.innerHeight);
     }
-}, 10);
-
+}, 1000);
 }
 
 
