db465bee53ff0daaa688c8622660d9c59c5fcbe5
sanjit-bhat <sanjit.bhat@gmail.com>
2019-02-24 17:52:37 -0500

    failure is upon us

--- js/gibber.js
+++ js/gibber.js
@@ -79,16 +79,19 @@ Gibber.init()
           if (i == 2) {c.kill()}
           if (notes[i] != -1) {
               if (i == 0) {
-                  a = FM('bass', { maxVoices:4, waveform:'PWM', attack:ms(1), decay:1000});
+                  a = FM('bass', { maxVoices:4, waveform:'PWM', attack:ms(1), decay:23});
                   a.note(notes[i], 1);
+                  console.log(notes[i])
               }
               if (i == 1) {
-                  b = Synth({ maxVoices:4, waveform:'PWM', attack:ms(1), decay:1000 });
+                  b = Synth({ maxVoices:4, waveform:'PWM', attack:ms(1), decay:23});
                   b.note(notes[i], 0.7);
+                  console.log(notes[i])
               }
               if (i == 2) {
-                  c = Synth({ maxVoices:4, waveform:'PWM', attack:ms(1), decay:1000});
+                  c = Synth({ maxVoices:4, waveform:'PWM', attack:ms(1), decay:23});
                   c.note(notes[i], 0.7);
+                  console.log(notes[i])
               }
           }
         }
