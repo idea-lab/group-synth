deaa53e6c7a28bbded859180f1aa2f286226a299
barinovi <igorb202@gmail.com>
2019-02-24 11:43:32 -0500

    added webcam files

--- cv.js
+++ cv.js
@@ -1,14 +1,14 @@
-const FLAG_NONE 			0;
-const FLAG_ORIENT_LEFT 		1;
-const FLAG_ORIENT_RIGHT 	2;
-const FLAG_ROT_LEFT			1;
-const FLAG_ROT_RIGHT		2;
-const FLAG_PUSH 			1;
+const FLAG_NONE =			0;
+const FLAG_ORIENT_LEFT = 	1;
+const FLAG_ORIENT_RIGHT =	2;
+const FLAG_ROT_LEFT =		1;
+const FLAG_ROT_RIGHT =		2;
+const FLAG_PUSH =			1;
 
 function normalize_raw_hsv(h, s, v)
 {
-    var normVals = [h,s,v];
-    normVals[0] = h * (127.0/360.0);
+    var normVals = [h,s,v, 0];
+    normVals[0] = h * (180.0/360.0);
     normVals[1] = s * (255.0 / 100.0);
     normVals[2] = v * (255.0 / 100.0);
     return normVals;
@@ -39,8 +39,8 @@ function bounds_height(bounds)
 function bounds_center(bounds)
 {
     return [
-        trunc((bounds[0] + bounds[2])/2),
-        trunc((bounds[1] + bounds[3])/2)
+        Math.trunc((bounds[0] + bounds[2])/2),
+        Math.trunc((bounds[1] + bounds[3])/2)
     ]
 }
 function get_bounds_rotation(current, old)
@@ -51,63 +51,74 @@ function get_bounds_rotation(current, old)
     deltaSide = bounds_width(current) - bounds_height(current);
     deltaSideOld = bounds_width(old) - bounds_height(old);
 
-    if (deltaSide > 0 and deltaSideOld < 0)
+    if (deltaSide > 0 && deltaSideOld < 0)
 	{	
-        if abs(deltaXMax) > abs(deltaXMin)
+        if (Math.abs(deltaXMax) > Math.abs(deltaXMin))
             return FLAG_ROT_LEFT;
-        if abs(deltaXMin) > abs(deltaXMax)
+        if (Math.abs(deltaXMin) > Math.abs(deltaXMax))
             return FLAG_ROT_RIGHT;
 	}
-    else if (deltaSide < 0 and deltaSideOld > 0)
+    else if (deltaSide < 0 && deltaSideOld > 0)
 	{
-        if abs(deltaXMax) > abs(deltaXMin):
+        if (Math.abs(deltaXMax) > Math.abs(deltaXMin))
             return FLAG_ROT_RIGHT;
-        if abs(deltaXMin) > abs(deltaXMax):
+        if (Math.abs(deltaXMin) > Math.abs(deltaXMax))
             return FLAG_ROT_LEFT;
 	}
 
     return FLAG_NONE;
 }
-function find_hand(image)
+function find_hand(image, mask)
 {
 	var imgHSV = new cv.Mat();
-	cv.cvtColor(image, imgHSV cv2.COLOR_BGR2HSV);
+	cv.cvtColor(image, imgHSV, cv.COLOR_RGB2HSV, 0);
     var colorMin = normalize_raw_hsv(0, 5, 0);
     var colorMax = normalize_raw_hsv(40, 100, 100);
-	var minMat = new cv.Mat(image.rows, image.cols, image.type(), colorMin);
-	var maxMat = new cv.Mat(image.rows, image.cols, image.type(), colorMax);
-	var mask = new cv.Mat();
-    cv2.inRange(imgHSV, minMat, maxMat, mask);
+	colorMax[3] = 255;
+	var minMat = new cv.Mat(imgHSV.rows, imgHSV.cols, imgHSV.type(), colorMin);
+	var maxMat = new cv.Mat(imgHSV.rows, imgHSV.cols, imgHSV.type(), colorMax);
+	//mask = new cv.Mat();
+    cv.inRange(imgHSV, minMat, maxMat, mask);
     //mask = cv2.blur(mask, (3, 3));
     //status, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY);
 	var contours = new cv.MatVector();
 	var harch = new cv.Mat();
 	
-	cv.findContours(mask, contours, harch, cv.RETR_COMP, cv.CHAIN_APPROX_SIMPLE);
+	cv.findContours(mask, contours, harch, Number(cv.RETR_COMP), Number(cv.CHAIN_APPROX_SIMPLE));
     var handContour = [];
 	
-	if (contours.length > 0)
+	if (contours.size() > 0)
 	{
-		countours.sort(function(a,b){
-			return b.cols - a.cols;
-		});
-		
-		handContour = countours[countours.length - 1];
-		
-		if (handContour.length > 0)
+		var maxIdx = 0;
+		var maxSize = contours.get(0).rows;
+		for(var i = 0; i < contours.size(); i++)
 		{
-			rc = cv2.boundingRect(handContour);
-			return make_bounds(rc[0], rc[1], rc[2], rc[3]);
+			if (contours.get(i).rows > maxSize)
+			{
+				maxSize = contours.get(i).rows;
+				maxIdx = i;
+			}
+		}
+		
+		handContour = contours.get(maxIdx);
+		
+		if (handContour.rows > 0)
+		{
+			rc = cv.boundingRect(handContour);
+			
+			minMat.delete();
+			maxMat.delete();
+			imgHSV.delete();
+			contours.delete();
+			harch.delete();
+			return make_bounds(rc.x, rc.y, rc.width, rc.height);
 		}
 	}
 	
+	minMat.delete();
+	maxMat.delete();
+	imgHSV.delete();
+	contours.delete();
+	harch.delete();
 	return null;
 }
-
-
-var video = document.getElementById('videoInput');
-var frame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-//var dst = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-var cap = new cv.VideoCapture(video);
-cap.read(frame);
-cv.imshow('canvasOutput', frame);
